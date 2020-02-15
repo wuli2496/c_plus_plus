d397400e1f8193a7a7d469c5eae847943a2704f0
wuli2496 <wuli2496@163.com>
2020-02-15 20:54:58 +0800

    修改排列算法，添加访问者的模板类型参数，达到找到一个排列后的处理自定义

--- src/algo/PermuAlgo.h
+++ src/algo/PermuAlgo.h
@@ -14,38 +14,50 @@
 using namespace std;
 
 template <typename T>
+class PermuVisitor
+{
+public:
+    void finish(const vector<T>& ans)
+    {
+
+    }
+};
+
+template <typename T, typename Visitor = PermuVisitor<T>>
 class PermuAlgo
 {
 public:
-    PermuAlgo(const vector<T>& in)
+    PermuAlgo(const vector<T>& in, const Visitor& visitor)
     {
         nums = in;
+        vis = visitor;
     }
 
-    void execute(vector<vector<T>>& ans)
+    void execute()
     {
-        dfs(0, ans);
+        dfs(0);
     }
 
 private:
-    void dfs(int cur, vector<vector<T>>& ans)
+    void dfs(int cur)
     {
-        if (cur == nums.size())
+        if (cur == static_cast<int>(nums.size()))
         {
-            ans.push_back(nums);
+            vis.finish(nums);
             return;
         }
 
         for (size_t i = cur; i < nums.size(); ++i)
         {
             swap(nums[cur], nums[i]);
-            dfs(cur + 1, ans);
+            dfs(cur + 1);
             swap(nums[cur], nums[i]);
         }
     }
 
 private:
     vector<T> nums;
+    Visitor vis;
 };
 
 #endif /* ALGO_PERMUALGO_H_ */
--- src/test/PermuAlgoTest.cpp
+++ src/test/PermuAlgoTest.cpp
@@ -9,25 +9,47 @@
 #include <vector>
 #include <cppunit/TestAssert.h>
 #include "../algo/PermuAlgo.h"
+#include <iostream>
 
 using namespace std;
 
 CPPUNIT_TEST_SUITE_REGISTRATION(PermuAlgoTest);
 
+class OutputVisitor
+{
+public:
+    OutputVisitor() : outstream(cout)
+    {
+
+    }
+    OutputVisitor(ostream& out) : outstream(out)
+    {
+
+    }
+
+    void finish(const vector<int>& ans)
+    {
+        for (size_t i = 0; i < ans.size(); ++i)
+        {
+            outstream << ans[i] << " ";
+        }
+        outstream << endl;
+    }
+
+    OutputVisitor operator=(const OutputVisitor& other)
+    {
+        return other;
+    }
+private:
+    ostream& outstream;
+};
+
 void PermuAlgoTest::testPermuAlgo()
 {
     vector<int> v = {1, 2, 3};
+    OutputVisitor outputVisitor(cout);
 
-    PermuAlgo<int> algo(v);
-    vector<vector<int>> ans;
-    algo.execute(ans);
+    PermuAlgo<int, OutputVisitor> algo(v, outputVisitor);
+    algo.execute();
 
-    for(auto& v : ans)
-    {
-        for (auto& n : v)
-        {
-            cout << n << " ";
-        }
-        cout << endl;
-    }
 }
